ecc745761a8775f2edd157df202f8ee78150ff30
cheshire394 <alicia.dsc94@gmail.com>
2025-01-13 10:41:49 +0100

    desplegar en git

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/game_trivial_vue.js/', // Reemplaza 'nombre-repo' con el nombre de tu repositorio
 })
+
+
