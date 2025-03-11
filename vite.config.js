fdb88e55e64416d74a4b07f316777f04a2e36d6f
cheshire394 <alicia.dsc94@gmail.com>
2025-03-11 19:23:55 +0100

    actualizo cambios de configuracion

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/game_trivial_vue.js/',  // 👈 este debe coincidir exactamente con el nombre del repositorio
   plugins: [vue()],
-  base: '/game_trivial_vue/', // Reemplaza 'nombre-repo' con el nombre de tu repositorio
 })
-
-
